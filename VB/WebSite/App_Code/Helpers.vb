ccf43c02fd91eb0bef2b4f92d746274f2d10a559
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/WebSite/App_Code/Helpers.vb
@@ -0,0 +1,23 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.Web
+Imports DevExpress.Web.ASPxScheduler
+Imports DevExpress.XtraScheduler
+
+Public Class ResourceFiller
+	Public Shared Users() As String = { "Sarah Brighton", "Ryan Fischer", "Andrew Miller" }
+	Public Shared Usernames() As String = { "sbrighton", "rfischer", "amiller" }
+
+	Public Shared Sub FillResources(ByVal storage As ASPxSchedulerStorage, ByVal count As Integer)
+		Dim resources As ResourceCollection = storage.Resources.Items
+		storage.BeginUpdate()
+		Try
+			Dim cnt As Integer = Math.Min(count, Users.Length)
+			For i As Integer = 1 To cnt
+				resources.Add(New Resource(Usernames(i - 1), Users(i - 1)))
+			Next i
+		Finally
+			storage.EndUpdate()
+		End Try
+	End Sub
+End Class
